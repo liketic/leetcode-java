8705aaa76212a868ce02cb2707e705717f70ca8b
Like <kel@splunk.com>
2017-03-18 15:57:40 +0800

    1. Two Sum

---
+++ src/TwoSum/Solution.java
@@ -0,0 +1,32 @@
+package TwoSum;
+
+
+/**
+ * Given an array of integers, return indices of the two numbers such that they add up to a specific
+ * target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same
+ * element twice.
+ *
+ * Example: Given nums = [2, 7, 11, 15], target = 9,
+ *
+ * Because nums[0] + nums[1] = 2 + 7 = 9, return [0, 1].
+ */
+public class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
+
+        int[] indices = new int[2];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                if (nums[i] + nums[j] == target) {
+                    indices[0] = i;
+                    indices[1] = j;
+                    return indices;
+                }
+            }
+        }
+        return null;
+    }
+}
