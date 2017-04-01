a915daf6e545f72cefb82f610d512e68881a973b
Like <kel@splunk.com>
2017-04-01 14:51:33 +0800

    40. Combination Sum II

---
+++ src/CombinationSumII/Solution.java
@@ -0,0 +1,72 @@
+package CombinationSumII;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a collection of candidate numbers (C) and a target number (T), find all unique combinations
+ * in C where the candidate numbers sums to T.
+ *
+ * Each number in C may only be used once in the combination.
+ *
+ * Note: All numbers (including target) will be positive integers. The solution set must not contain
+ * duplicate combinations. For example, given candidate set [10, 1, 2, 7, 6, 1, 5] and target 8, A
+ * solution set is:
+ * <pre>
+ * [
+ *  [1, 7],
+ *  [1, 2, 5],
+ *  [2, 6],
+ *  [1, 1, 6]
+ * ]
+ * </pre>
+ */
+public class Solution {
+
+    private List<List<Integer>> dfs(int[] candidates, int[] count, int target, int pos, List<Integer> nums) {
+        List<List<Integer>> rv = new ArrayList<>();
+        if (pos == -1) {
+            if (target == 0) {
+                rv.add(nums);
+            }
+            return rv;
+        }
+        if (target < 0) return rv;
+        for (int i = 0; i <= count[pos]; i++) {
+            if (i > 0) {
+                nums.add(candidates[pos]);
+                target -= candidates[pos];
+                if (target < 0) break;
+            }
+            rv.addAll(dfs(candidates, count, target, pos - 1, new ArrayList<>(nums)));
+        }
+        return rv;
+    }
+
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        int n = candidates.length;
+        if (n == 0) {
+            return Collections.emptyList();
+        }
+
+        Arrays.sort(candidates);
+
+        int[] nums = new int[n];
+        int[] count = new int[n];
+        int k = 0;
+        nums[0] = candidates[0];
+        count[0] = 1;
+
+        for (int i = 1; i < n; i++) {
+            if (candidates[i] != nums[k]) {
+                nums[++k] = candidates[i];
+                count[k] = 1;
+            } else {
+                count[k]++;
+            }
+        }
+        return dfs(nums, count, target, k, new ArrayList<>());
+    }
+}
