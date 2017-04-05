b9e02e991af15489c5f6b8efc9ce739ee330be82
Like <kel@splunk.com>
2017-04-05 20:14:31 +0800

    58. Length of Last Word

---
+++ src/LengthofLastWord/Solution.java
@@ -0,0 +1,31 @@
+package LengthofLastWord;
+
+/**
+ * Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return
+ * the length of last word in the string.
+ *
+ * If the last word does not exist, return 0.
+ *
+ * Note: A word is defined as a character sequence consists of non-space characters only.
+ *
+ * <pre>
+ * For example,
+ * Given s = "Hello World",
+ * return 5.
+ * </pre>
+ */
+public class Solution {
+    public int lengthOfLastWord(String s) {
+        int n = s.length();
+        int count = 0;
+
+        for (int i = n - 1; i >= 0; i--) {
+            if (Character.isWhitespace(s.charAt(i))) {
+                if (count > 0) break;
+            } else {
+                count++;
+            }
+        }
+        return count;
+    }
+}
