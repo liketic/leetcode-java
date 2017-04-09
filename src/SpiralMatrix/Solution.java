4b25e250bac50e19824e629bb6b0abf45f95dc7c
Like <kel@splunk.com>
2017-04-09 11:38:12 +0800

    54. Spiral Matrix

---
+++ src/SpiralMatrix/Solution.java
@@ -0,0 +1,70 @@
+package SpiralMatrix;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Given a matrix of m x n elements (m rows, n columns), return all elements of the matrix in spiral
+ * order.
+ *
+ * For example, Given the following matrix:
+ *
+ * <pre>
+ * [
+ * [ 1, 2, 3 ],
+ * [ 4, 5, 6 ],
+ * [ 7, 8, 9 ]
+ * ]
+ * </pre>
+ * You should return [1,2,3,6,9,8,7,4,5].
+ */
+public class Solution {
+    public List<Integer> spiralOrder(int[][] matrix) {
+        int m = matrix.length;
+        if (m == 0) {
+            return Collections.emptyList();
+        }
+        int n = matrix[0].length;
+        List<Integer> rv = new ArrayList<>();
+
+        int r = 0, c = 0;
+
+        while (n > 0 && m > 0) {
+            for (int i = c; i < c + n; i++) {
+                rv.add(matrix[r][i]);
+            }
+            for (int j = r + 1; j < r + m; j++) {
+                rv.add(matrix[j][c + n - 1]);
+            }
+            if (m > 1) {
+                for (int i = c + n - 2; i >= c; i--) {
+                    rv.add(matrix[r + m - 1][i]);
+                }
+            }
+            if (n > 1) {
+                for (int j = r + m - 2; j > r; j--) {
+                    rv.add(matrix[j][c]);
+                }
+            }
+            n -= 2;
+            m -= 2;
+            r++;
+            c++;
+        }
+
+        return rv;
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] mat = new int[1][3];
+        mat[0][0] = 8;
+        mat[0][1] = 7;
+        mat[0][2] = 6;
+
+        for (int x : solution.spiralOrder(mat)) {
+            System.out.println(x);
+        }
+    }
+}
