ba7649b7bdf9b07a070a051e0b1e97ebaea99701
Like <kel@splunk.com>
2017-04-09 11:56:23 +0800

    55. Jump Game

---
+++ src/JumpGame/Solution.java
@@ -0,0 +1,32 @@
+package JumpGame;
+
+
+/**
+ * Given an array of non-negative integers, you are initially positioned at the first index of the
+ * array.
+ *
+ * Each element in the array represents your maximum jump length at that position.
+ *
+ * Determine if you are able to reach the last index.
+ *
+ * For example:
+ * <pre>
+ * A = [2,3,1,1,4], return true.
+ *
+ * A = [3,2,1,0,4], return false.
+ * </pre>
+ */
+public class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int maxPos = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || i <= maxPos) {
+                maxPos = Math.max(maxPos, i + nums[i]);
+            }
+        }
+
+        return maxPos >= n - 1;
+    }
+}
