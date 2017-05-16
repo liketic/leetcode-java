7af04d37e48629bba73a512dfd6a749605930c8d
Like <kel@splunk.com>
2017-05-16 10:47:05 +0800

    448. Find All Numbers Disappeared in an Array

---
+++ src/FindAllNumbersDisappearedinanArray/Solution.java
@@ -0,0 +1,50 @@
+package FindAllNumbersDisappearedinanArray;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Given an array of integers where 1 ≤ a[i] ≤ n (n = size of array), some elements appear twice and
+ * others appear once.
+ *
+ * Find all the elements of [1, n] inclusive that do not appear in this array.
+ *
+ * Could you do it without extra space and in O(n) runtime? You may assume the returned list does
+ * not count as extra space.
+ *
+ * Example:
+ * <pre>
+ * Input:
+ * [4,3,2,7,8,2,3,1]
+ *
+ * Output:
+ * [5,6]
+ * </pre>
+ */
+public class Solution {
+
+    private int locationOf(int v) {
+        return v - 1;
+    }
+
+    public List<Integer> findDisappearedNumbers(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            int n = nums[i];
+            while (true) {
+                int l = locationOf(n);
+                if (nums[l] == n) break;
+                int t = nums[l];
+                nums[l] = n;
+                n = t;
+                if (l == i) break;
+            }
+        }
+        List<Integer> disappeared = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (locationOf(nums[i]) != i) {
+                disappeared.add(i + 1);
+            }
+        }
+        return disappeared;
+    }
+}
